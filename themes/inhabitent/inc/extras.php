ee522518a343e4301f66003a150dd4965f4b1fa4
Supreet Chahal <supreet_chahal@hotmail.com>
2017-03-07 10:50:11 -0800

    Fixed login screen logo

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -29,7 +29,7 @@ add_filter( 'body_class', 'red_starter_body_classes' );
 function my_login_logo() { ?>
     <style type="text/css">
         body.login div#login h1 a{
-				background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/inhabitent-logo-text-dark.svg);
+				background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-text-dark.svg);
 				 background-position: center center;
 				 background-repeat: no-repeat;
 				 background-size: 250px 250px;
